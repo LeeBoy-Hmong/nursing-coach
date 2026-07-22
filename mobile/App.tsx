2089c00fb3e1e0b208fec33fbcda6b429ff525bf
Michael Lee <michael1998lee@outlook.com>
2026-07-22 17:57:26 -0500

    Created the basics to a login screen. Understanding TypeScript.

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useState } from 'react';
+
+  // Defining the shape of my data. user 'interface' to build it out - similar to BaseModel in Pydantic.
+  interface User {
+    name: string;
+    isLoggedIN: boolean;
+  };
 
 export default function App() {
+
+  const [user, setUser] = useState<User>({ name: "Michael", isLoggedIN: false});
+  
+  function loggedIN() {
+    setUser({ ...user, isLoggedIN: true });  // '...user' carries over the mutable data that was set.
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Welcome: {user.name}</Text>
+
+      {user.isLoggedIN ? (
+        <Text>You are logged in!</Text>
+      ) : (
+        <Button title='Log in' onPress={loggedIN} />
+      )
+    }
       <StatusBar style="auto" />
     </View>
   );

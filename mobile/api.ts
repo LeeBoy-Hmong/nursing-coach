6474a32d10d18e757b6db3f27ce6098377801add
Michael Lee <michael1998lee@outlook.com>
2026-07-30 18:48:29 -0500

    Set up the fetch function here -- added a variable to make alternating APP URL simpler in the future -- created interface here as well.

---
+++ mobile/api.ts
@@ -0,0 +1,17 @@
+export interface Question {
+    question: string;
+    answer: string;
+}
+
+const API_URL = "http://localhost:8000"
+
+export async function fetchQuiz(nurseTopic: string): Promise<Question[]> {
+    const response = await fetch(`${API_URL}/claude-quiz`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ notes: nurseTopic }),
+    });
+    if (!response.ok) throw new Error(`Status: ${response.status}`);  // Throw a status error if issue with fetching from the API.
+    const data = await response.json();
+    return data["claude's reply"];
+}

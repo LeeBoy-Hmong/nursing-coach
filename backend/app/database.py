aa3c430e35114e7feb86ef95d2869f2b85ff4dbd
Michael Lee <michael1998lee@outlook.com>
2026-08-03 14:19:28 -0500

    Building out the engine in database.py & fixed the data schema in models.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,1 +1,11 @@
-## Database tables
+## Database tables -- This is where the settings are read.
+## Where Python applications initialzie and centralize DB connections and session factories.
+## Uses the SQL Alchemy toolkit.
+from sqlalchemy.ext.asyncio import create_async_engine
+from app.config import settings
+
+# The Engine - module level & created once.
+engine = create_async_engine(settings.supabase_connetor, echo=True)  # remove echo when pushing to production.
+# Session factory - template for making sessions.
+
+# Dependency FastAPI injects
---
+++ backend/app/models.py
@@ -0,0 +1,47 @@
+## This is the single source of truth for all of our data shape.
+import uuid
+from typing import ClassVar
+from datetime import datetime
+from sqlmodel import SQLModel, Field
+
+class StudyItem(SQLModel, table=True):
+    __tablename__: ClassVar[str] = "study_item"  # Change the tablename is now generated as "study_item" instead of "studyitem" (typical naming conven of SQLMol)
+
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    user_id: uuid.UUID | None = None # The field is nullable (uuid.UUID | None) - if an input is not provided, the last "None" states it as optional to fill in.
+    type: str
+    title: str
+    topic: str | None = None
+    source_type: str = "ai_generated"
+    created_at: datetime | None = None  # Database will fill these automatically.
+    updated_at: datetime | None = None
+
+class QuizQuestion(SQLModel, table=True):
+    __tablename__: ClassVar[str] = "quiz_question"
+
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    study_item_id: uuid.UUID = Field(foreign_key="study_item.id")
+    question: str
+    answer: str
+    position: int
+
+class MedCard(SQLModel, table=True):
+    __tablename__: ClassVar[str] = "med_card"
+
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    study_item_id: uuid.UUID = Field(foreign_key="study_item.id")
+    generic_name: str
+    brand_name: str | None = None
+    drug_class: str | None = None
+    dose: str | None = None
+    route: str | None = None
+    frequency: str | None = None
+    mechanism_of_action: str | None = None 
+    contraindications: str | None = None
+    adverse_effects: str | None = None
+    nursing_considerations: str | None = None
+    patient_teaching: str | None = None
+    labs_to_monitor: str | None = None
+    rxcui: str | None = None
+    external_verified_at: datetime | None = None
+    indication: str | None = None

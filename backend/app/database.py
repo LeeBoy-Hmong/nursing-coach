594a56eefc3682dc8eaa9203dd048dc4c78dcffd
Michael Lee <michael1998lee@outlook.com>
2026-08-04 21:02:54 -0500

    Added comment on the AsyncGenerator toolkit.

--- backend/app/database.py
+++ backend/app/database.py
@@ -14,7 +14,7 @@ async_sess = async_sessionmaker(engine, expire_on_commit=False)
 # Dependency FastAPI injects - create an asynchronous 'get_session()' function
 '''Instead of opening a connection and relying on the framework to remember to close it, write a Python generator function using yield.
 This function handles the setup, hands over the session to your API route, and automatically runs cleanup code afterward.'''
-async def get_session() -> AsyncGenerator[AsyncSession, None]:
+async def get_session() -> AsyncGenerator[AsyncSession, None]:  # AsyncGenerator tells Pylance to use async and yield
     database = async_sess()
     async with database:
         yield database

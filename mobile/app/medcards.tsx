a730272d6c729e7828cc94771bcabb2728f2478b
Michael Lee <michael1998lee@outlook.com>
2026-09-11 21:35:50 -0500

    Imported in Screen form MedicalCard.tsx to my app exporouter.

--- mobile/app/medcards.tsx
+++ mobile/app/medcards.tsx
@@ -1,20 +1,5 @@
-import {View, Text, FlatList } from 'react-native';
-import { useLocalSearchParams, Link } from 'expo-router';  // The tappable modules that allow switching between URL
+import MedicalCards from "../src/screens/MedicalCard";
 
-// Create an exportable function RecipeList()
-export default function RecipeList() {
-    const recipes = [
-        { id: '1', name: 'Carbonara'},
-        { id: '2', name: 'Heath'},
-    ];
-
-    return (
-        <View>
-            {recipes.map((recipe) => (
-                <Link key={recipe.id} href={`/medcards/${recipe.id}`}>
-                    <Text>{recipe.name}</Text>
-                </Link>
-            ))}
-        </View>
-    )
+export default function MedCardList() {
+    return <MedicalCards />
 }

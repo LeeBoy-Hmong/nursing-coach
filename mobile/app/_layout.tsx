62086cd6352a8620e0b4c0388a9c597809904b8f
Michael Lee <michael1998lee@outlook.com>
2026-09-24 00:39:43 -0500

    Removed Header Titles

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -3,7 +3,8 @@ import { Stack } from 'expo-router';
 // Export a default function called a RootLayout.
 export default function RootLayout() {
     return (
-        <Stack>
+        // user 'screenOptions={{ headerShown: false }} to get rid of the all the ugly titles.
+        <Stack screenOptions={{ headerShown: false}}> 
             <Stack.Screen name="index" options={{ title: "Home"}} />
         </Stack>
     );
